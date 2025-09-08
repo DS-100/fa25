ceecbb0db5b65ed1aba417f26dbdad3e14569a88
James Geronimo <jegeronimo@berkeley.edu>
2025-09-08 16:54:11 -0700

    [edit] lecture hyperlinking logic

--- _plugins/asset_tags.rb
+++ _plugins/asset_tags.rb
@@ -40,17 +40,10 @@ module Jekyll
       lectures = context['site']['lectures']
       lecture = lectures[@number.to_i - 1] if lectures[@number.to_i - 1]
       lecture_title = lecture['title']
-      # Directly grab the date string from the lecture data
-      date_parts = lecture['date'].to_s.split('-')
-      year = date_parts[0].to_i
-      month = date_parts[1].to_i
-      day = date_parts[2].to_i
-      # Construct the date manually
-      lecture_date = Date.new(year, month, day)
-      current_date = Date.today
       # add leading 0 to number if less than 10
       num_index = @number.to_i < 10 ? "0#{@number}" : @number
-      return "**Lecture #{@number}**{: .label .label-lecture } #{lecture_title}" if lecture_date > current_date
+      # Check if hyperlinked field is true, if not return plain text
+      return "**Lecture #{@number}**{: .label .label-lecture } #{lecture_title}" unless lecture['hyperlinked']
 
       "**Lecture #{@number}**{: .label .label-lecture } [#{lecture_title}](lectures/#{num_index})"
     end
